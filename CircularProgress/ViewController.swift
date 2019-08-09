3100f9fd84192a6e959eda4967e9c1516467aff8
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2019-08-09 10:44:11 -0300

    Initial Commit

---
+++ CircularProgress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircularProgress
+//
+//  Created by Marcelo Sampaio on 09/08/19.
+//  Copyright © 2019 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
